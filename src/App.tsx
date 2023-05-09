831b2ec6b183d55a398428edc4900a00e61b311a
dkzhen <dani314123@gmail.com>
2023-05-09 21:45:48 +0700

    responsive nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <div>
-        <div className="">
+        <div className="hidden md:block">
           <Navbar />
         </div>
 
